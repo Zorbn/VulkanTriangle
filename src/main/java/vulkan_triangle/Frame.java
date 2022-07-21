d36292ad7831080bf15be53c5933e86d3f4dc8eb
Zorbn <public.zorb@gmail.com>
2022-07-20 19:32:54 -0700

    Initial commit

---
+++ src/main/java/vulkan_triangle/Frame.java
@@ -0,0 +1,23 @@
+package vulkan_triangle;
+
+import java.nio.LongBuffer;
+
+import static org.lwjgl.system.MemoryStack.stackGet;
+
+/*
+ * Wrapper for the sync objects of an in-flight frame.
+ * The sync objects must be deleted manually!
+ */
+public record Frame(long imageAvailableSemaphore, long renderFinishedSemaphore, long fence) {
+    public LongBuffer pImageAvailableSemaphore() {
+        return stackGet().longs(imageAvailableSemaphore);
+    }
+
+    public LongBuffer pRenderFinishedSemaphore() {
+        return stackGet().longs(renderFinishedSemaphore);
+    }
+
+    public LongBuffer pFence() {
+        return stackGet().longs(fence);
+    }
+}
